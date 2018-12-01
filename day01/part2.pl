a5b30f80692e21c2d5702fbee11e2bd1e15b2ac5
Michael Mraz <michaelmraz+github@gmail.com>
2018-12-01 00:37:30 -0600

    Strip unnecessary prints from day 1 part 2

--- day01/part2.pl
+++ day01/part2.pl
@@ -1,23 +1,23 @@
 #!/usr/bin/env perl
 use List::MoreUtils qw( firstidx );
 
-open(FIN, "<inputs/in");
 my $freq = 0;
 my @changes;
 my @freqs;
 push @freqs, $freq;
+
+open(FIN, "<inputs/in");
 while(<FIN>) {
     chomp;
     push @changes, $_;
 }
+close(FIN);
 
 my $idx = -1;
 while( $idx == -1 ) {
     foreach(@changes) {
         $freq += $_;
-        print "c: $freq\n";
         $idx = firstidx { $_ == $freq } @freqs;
-        print "i: $idx\n";
         if( $idx >= 0 ) {
             print "$freq\n";
             last;
