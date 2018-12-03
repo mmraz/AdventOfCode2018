d32975d8e907ce6e2b4a5149bc941d429b4becd6
Michael Mraz <michaelmraz+github@gmail.com>
2018-12-03 01:30:16 -0600

    Day 3 solutions

--- day03/part1.pl
+++ day03/part1.pl
@@ -10,7 +10,7 @@ close(FIN);
 
 my %cloth;
 for my $claim (@inputs) {
-    $claim =~ /@ (\d+),(\d+): (\d+)x(\d+)/;
+    $claim =~ m/@ (\d+),(\d+): (\d+)x(\d+)/;
     for my $h ($1..$1+$3-1) {
         for my $v ($2..$2+$4-1) {
             $cloth{"$h,$v"}++;
