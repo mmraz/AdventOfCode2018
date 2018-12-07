bbe5ca433a116fb091086eea0ade4165493a4d47
Michael Mraz <michaelmraz+github@gmail.com>
2018-12-06 21:08:59 -0600

    Day 6 solutions

---
+++ day06/part1.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/env perl
+use List::MoreUtils qw(zip6);
+use List::Util qw(max sum);
+use Data::Dumper qw(Dumper);
+$Data::Dumper::Indent = 1;
+
+my $DEBUG = 0;
+
+open(FIN, "<inputs/in");
+my @x, @y, @grid, @inputs;
+while(<FIN>) {
+    chomp;
+    (my $xin, my $yin) =  split(/, /, $_);
+    push(@x, $xin);
+    push(@y, $yin);
+}
+close(FIN);
+
+@inputs = zip6(@x, @y);
+my $max_x = max(@x);
+my $max_y = max(@y);
+
+print Dumper \@inputs if $DEBUG;
+
+my $grid = [];
+
+foreach my $x (0 .. $max_x) {
+    foreach my $y (0 .. $max_y) {
+        my @distances =
+            sort {$$a[1] <=> $$b[1]}
+            map  {[$_, abs ($inputs[$_][0] - $x) + abs ($inputs[$_][1] - $y)]} keys @inputs;
+        if ($distances[0][1] == $distances[1][1]) {
+            $$grid[$x][$y] = ".";
+        }
+        else {
+            $$grid[$x][$y] = $distances[0][0];
+        }
+    }
+}
+
+my %ignore;
+foreach my $x (0, $max_x) {
+    foreach my $y (0 .. $max_y) {
+        $ignore {$$grid[$x][$y]} = 1;
+    }
+}
+foreach my $x (0 .. $max_x) {
+    foreach my $y (0, $max_y) {
+        $ignore {$$grid[$x][$y]} = 1;
+    }
+}
+
+foreach my $x (0 .. $max_x) {
+    foreach my $y (0 .. $max_y) {
+        $$grid[$x][$y] = "x" if $ignore {$$grid[$x][$y]};
+    }
+}
+
+my %counts;
+foreach my $row (@$grid) {
+    foreach my $cell (@$row) {
+        $counts {$cell} ++;
+    }
+}
+delete $counts {"x"};
+
+print Dumper \%counts if $DEBUG;
+print max(values %counts) . "\n";
