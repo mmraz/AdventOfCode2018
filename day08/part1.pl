3d5e59d86a50d2e4ac96a3e18360989bac566df4
Michael Mraz <michaelmraz+github@gmail.com>
2018-12-08 00:40:52 -0600

    Day 8 part 1

---
+++ day08/part1.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+use List::Util qw(sum);
+use Data::Dumper qw(Dumper);
+$Data::Dumper::Indent = 1;
+my $DEBUG = 0;
+
+open(FIN, "<inputs/in");
+my @inputs;
+while(<FIN>) {
+    chomp;
+    @inputs = split(/ /, $_);
+}
+close(FIN);
+
+print Dumper \@inputs if $DEBUG;
+
+my $children = 0;
+my $metas = 0;
+my @metadata;
+
+sub tree_build {
+    my $children = shift @inputs;
+    my $metas = shift @inputs;
+
+    for(1..$children) {
+        tree_build();
+    }
+    my @meta = splice @inputs, 0, $metas;
+    push(@metadata,@meta);
+}
+
+tree_build();
+print sum(@metadata) . "\n";
